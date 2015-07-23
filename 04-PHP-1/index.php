0da516549f6e40016b92e0aa6d4c401d676b6f55
Hristo <Hristo@PC>
2015-07-23 21:48:19 +0300

    First PHP practice

---
+++ 04-PHP-1/index.php
@@ -0,0 +1,52 @@
+<?php
+	$my_name = "Hristo";
+	$last_name = "Bahnev";
+	echo $my_name." ".$last_name;
+	
+	echo"<br />";
+	
+	$day_of_month = 23;
+	
+	if($day_of_month < 25 || $day_of_month == 23)
+	{
+		echo "Далече сме от заплата ";
+	}
+	else
+	{
+		echo "Заплатата е близо";
+	}
+	
+	echo"<br />";
+	
+	$x = 10;
+	$z = 20;
+	
+	$q=4;
+	$q+=$x;
+	echo $q;
+	
+	echo"<br />";
+	
+	$w = 7;
+	$w -=$z;
+	echo $w;
+	
+	echo"<br />";
+	
+	$r = 100000;
+	$r*=$x;
+	echo $r;
+	
+	echo"<br />";
+	
+	$t = 200;
+	$t/=$r;
+	echo $t;
+	
+	
+	
+	
+	
+	
+	
+?>
