0744186510cf8673a9832f5f997da8294d3e8f87
Hristo <Hristo@PC>
2015-07-25 20:55:00 +0300

    Не съм използвал "if-else" умишлено, защото програмата нямаше да изпълни всички if-ове, а в случая е възможен повече от един.

---
+++ 04-PHP-1-homework/Homework 1/homework1.php
@@ -0,0 +1,35 @@
+<?php
+$a = 64;
+$red_text = "<div style=background-color:red>По-малко или равно на 64</div>";
+if($a==15)
+{
+	echo "Аз съм 15";
+}
+if($a>26)
+{
+	echo "По-голяма от 26";
+}
+if ($a<=64)
+{
+	echo $red_text;
+}
+
+$a = 15;
+$red_text = "<div style=background-color:red>По-малко или равно на 64</div>";
+if($a==15)
+{
+	echo "Аз съм 15";
+}
+if($a>26)
+{
+	echo "По-голяма от 26";
+}
+if ($a<=64)
+{
+	echo $red_text;
+}
+
+
+
+
+?>
