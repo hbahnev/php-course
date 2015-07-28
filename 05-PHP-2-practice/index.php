afd28700f0ec3f58a2c6651caf2b7ce53e776458
Hristo <Hristo@PC>
2015-07-28 21:36:39 +0300

    PHP-2 practice

---
+++ 05-PHP-2-practice/index.php
@@ -0,0 +1,45 @@
+<?php
+
+$a = 'Hristo';
+$b = "Аз съм <b>$a</b>";
+
+echo $b;
+echo "<br />";
+echo strlen($b);
+echo "<br />";
+echo mb_strlen($b);
+echo "<br />";
+
+
+
+
+$counter = 1;
+	while($counter < 10){
+		echo $counter.'</br>';
+		$counter++;
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
